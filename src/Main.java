3253acd993fab8f6751526f36a7a8ef2c5b1cb67
yayalebledardt <yanispro917@gmail.com>
2026-04-13 13:45:40 +0200

    Mission 1

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    static void main() {
+
+        String marque = "peugeot";
+        int modele =  208;
+        int année = 2021;
+        int prix = 14500;
+
+        System.out.println("marque : "+marque);
+        System.out.println("année : "+modele);
+        System.out.println("année : "+année);
+        System.out.println("prix : "+prix);
+
+
+    }
+}
